3dfe290833be2ccd0982a31ab57941bf5907ee01
Brian Sanchez <briansanchez@Brians-MacBook-Air.local>
2023-05-21 13:21:16 -0400

    Set up DB connection.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const eventRoutes = require('./routes/events')
+
+// Env vars
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
 
 // Express app
 const app = express()
-
-// Port
-const port = process.env.PORT
 
 // Middleware
 app.use(express.json())
@@ -20,7 +22,15 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/events', eventRoutes)
 
-// Listen for requests
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-})
+/// Connect to database
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        console.log('Connected to database')
+        // Listen for requests
+        app.listen(PORT, () => {
+        console.log('Listening on port ' + PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
