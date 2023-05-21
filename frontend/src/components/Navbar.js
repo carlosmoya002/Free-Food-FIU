7a621c13f12a6ab4446fb11f738a3efc3577540c
Annie <anguy042@fiu.edu>
2023-05-21 14:56:50 -0400

    Front End Start

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+
+    return(
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>Free Food FIU</h1>
+                </Link>
+            </div>
+        </header>
+
+    )
+
+}
+
+export default Navbar
