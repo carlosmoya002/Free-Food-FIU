bd738d8df0ff513c17889ab719ddd6a82d1db651
Brian Sanchez <brian.sanchez.main@gmail.com>
2023-07-01 15:58:55 -0400

    Fix registration (login needs work).

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -4,7 +4,7 @@ const User = require('../models/userModel');
 
 const registerUser = async (req, res) => {
 
-    const { email, password, preferences } = req.body;
+    const { email, firstName, lastName, password, preferences } = req.body;
 
     try {
 
@@ -20,10 +20,8 @@ const registerUser = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, costFactor);
 
         // Create user
-        const newUser = new User({ email, password: hashedPassword, preferences });
-        await newUser.save();
-
-        res.status(201).json({ message: 'User created successfully' });
+        const user = await User.create({email, firstName, lastName, password: hashedPassword, preferences});
+        res.status(200).json({user})
 
     } catch (error) {
         res.status(500).json({ message: 'Registration failed' });
