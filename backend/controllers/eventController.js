4c92708b05a9640d3947603d222dc2f314a2af93
carlosmoya002 <83508169+carlosmoya002@users.noreply.github.com>
2023-07-18 21:18:09 -0400

    Update eventController.js

--- backend/controllers/eventController.js
+++ backend/controllers/eventController.js
@@ -1,48 +1,66 @@
 import asyncHandler from 'express-async-handler';
 import Event from '../models/eventModel.js';
+import { upload } from '../middleware/fileUploadMiddleware.js';
 
-// @desc    Add event
+// @desc    Add event with image upload
 // @route   POST /api/events/addEvent
-// @access  Public
+// @access  Private
 const addEvent = asyncHandler(async (req, res) => {
-  const {
-    name,
-    date,
-    time,
-    location,
-    organizer,
-    description,
-    preferences,
-    allergies,
-  } = req.body;
+  upload.single('image')(req, res, async (err) => {
+    if (err) {
+      res.status(400);
+      throw new Error('Image upload failed');
+    }
 
-  const event = await Event.create({
-    name,
-    date,
-    time,
-    location,
-    organizer,
-    description,
-    preferences,
-    allergies,
-  });
+    const {
+      name,
+      date,
+      time,
+      location,
+      organizer,
+      description,
+      preferences,
+      allergies,
+    } = req.body;
 
-  if (event) {
-    res.status(201).json({
-      _id: event._id,
-      name: event.name,
-      date: event.date,
-      time: event.time,
-      location: event.location,
-      organizer: event.organizer,
-      description: event.description,
-      preferences: event.preferences,
-      allergies: event.allergies,
+    // Get the uploaded image file name with extension
+    const imageName = req.file ? req.file.filename : undefined;
+
+    // Get the user ID from the authenticated request
+    const userId = req.user._id;
+
+    const event = await Event.create({
+      name,
+      date,
+      time,
+      location,
+      organizer,
+      description,
+      preferences,
+      allergies,
+      image: imageName,
+      user: userId, // Store the user's ID with the event data
     });
-  } else {
-    res.status(400);
-    throw new Error('Invalid event data');
-  }
+
+    if (event) {
+      res.status(201).json({
+        _id: event._id,
+        name: event.name,
+        date: event.date,
+        time: event.time,
+        location: event.location,
+        organizer: event.organizer,
+        description: event.description,
+        preferences: event.preferences,
+        allergies: event.allergies,
+        image: event.image,
+        user: event.user, // Include the user ID in the response for reference
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid event data');
+    }
+  });
 });
 
 // @desc    Update event
@@ -122,7 +140,6 @@ const getEventsByField = asyncHandler(async (req, res) => {
         query = { organizer: { $regex: new RegExp(value, 'i') } };
         break;
       // Add more cases for other fields as needed
-
       default:
         return res.status(400).json({ error: 'Invalid field' });
     }
@@ -131,6 +148,22 @@ const getEventsByField = asyncHandler(async (req, res) => {
     res.status(200).json(events);
   } catch (error) {
     res.status(400).json({ error: error.message });
+  }
+});
+
+// @desc    Get events by user ID
+// @route   GET /api/events/user
+// @access  Private
+const getEventsByUser = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const events = await Event.find({ user: userId });
+
+  if (events) {
+    res.json(events);
+  } else {
+    res.status(404);
+    throw new Error('No events found for this user');
   }
 });
 
@@ -155,5 +188,6 @@ export {
   getEvent,
   getAllEvents,
   getEventsByField,
+  getEventsByUser,
   deleteEvent,
 };
