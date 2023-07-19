7734b899c0b7016c1f3af365134db77edd71fe4d
carlosmoya002 <83508169+carlosmoya002@users.noreply.github.com>
2023-07-18 21:19:55 -0400

    Update eventModel.js

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -1,18 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-
-const eventSchema = mongoose.Schema({
-    name: {type: String, requied: true},
-    date: {type: String, require: true},
-    time: {type: String, required: true},
-    location: {type: String, requied: true},
-    organizer: {type: String, requied: true},
-    description: {type: String, requied: true},
-    preferences: {type: [String], required: true},
-    allergies: {type: [String], required: true},
-}, {
-    timestamps: true
-});
+const eventSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    date: { type: String, require: true },
+    time: { type: String, required: true },
+    location: { type: String, requied: true },
+    organizer: { type: String, requied: true },
+    description: { type: String, requied: true },
+    preferences: { type: [String], required: true },
+    allergies: { type: [String], required: true },
+    image: { type: String},
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}, 
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Event = mongoose.model('Event', eventSchema);
 
