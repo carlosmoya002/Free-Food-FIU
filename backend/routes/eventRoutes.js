150e9caaff3674525395679dcd89ce2b014cc887
carlosmoya002 <83508169+carlosmoya002@users.noreply.github.com>
2023-07-18 21:24:33 -0400

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,20 +1,23 @@
 import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
 import { 
     addEvent,
     updateEvent,
     getEvent,
     getAllEvents,
     getEventsByField,
-    deleteEvent
+    deleteEvent,
+    getEventsByUser,
  } from '../controllers/eventController.js';
 
 const router = express.Router();
 
-router.post('/addEvent', addEvent);
-router.put('/updateEvent', updateEvent);
+router.post('/addEvent', protect, addEvent);
+router.put('/updateEvent', protect, updateEvent); 
 router.get('/getEvent', getEvent);
+router.get('/user', protect, getEventsByUser); 
 router.get('/', getAllEvents);
 router.get('/:field/:value', getEventsByField);
-router.delete('/deleteEvent', deleteEvent);
+router.delete('/deleteEvent', protect, deleteEvent);
 
 export default router;
