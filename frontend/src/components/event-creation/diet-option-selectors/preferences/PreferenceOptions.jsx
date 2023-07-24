d142f598b59c010558f9555325a5fd4c54596e61
carlosmoya002 <83508169+carlosmoya002@users.noreply.github.com>
2023-07-24 14:22:54 -0400

    Delete frontend directory

--- frontend/src/components/event-creation/diet-option-selectors/preferences/PreferenceOptions.jsx
+++
@@ -1,7 +0,0 @@
-const PreferenceOptions = [
-  { value: 'vegetarian', label: 'Vegetarian' },
-  { value: 'vegan', label: 'Vegan' },
-  { value: 'kosher', label: 'Kosher' },
-];
-
-export default PreferenceOptions;
