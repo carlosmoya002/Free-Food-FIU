0615e285f4721ab68a74f7bb7ca0b745f4f9024b
carlosmoya002 <83508169+carlosmoya002@users.noreply.github.com>
2023-07-24 02:57:21 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import dotenv from "dotenv";
+
+const envFilePath = path.resolve("../", ".env");
+dotenv.config({ path: envFilePath });
+
+const port = process.env.PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,8 +14,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: `http://localhost:${port}`,
         changeOrigin: true,
       },
     },
